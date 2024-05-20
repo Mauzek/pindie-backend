58fc4f8c1dc6b024b97cbadbbbbb6bf0d4aebd15
Mauzek <artemilliy@gmail.com>
2024-05-20 23:57:52 +0300

    Добавлен адрес cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -6,7 +6,8 @@ const allowedCors = [
   "https://practicum.yandex.ru",
   "https://students-projects.ru",
   "http://localhost:3000",
-  "http://localhost:3001"
+  "http://localhost:3001",
+  "https://pindiegames.nomoredomainswork.ru"
 ];
 
 const cors = (req, res, next) => {
